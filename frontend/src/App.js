dd51dc53dbb02a1190055af1e9b432b0a87b5025
GreyHatHacker237 <audreymandeng13@gmail.com>
2025-05-20 11:26:53 +0200

    Création du composant CurrencyConverter de base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 // frontend/src/App.js
 import './App.css';
+import CurrencyConverter from './components/CurrencyConverter';
 
 function App() {
   return (
     <div className="App">
       <h1>Convertisseur de Devise</h1>
+      <CurrencyConverter />
     </div>
   );
 }
